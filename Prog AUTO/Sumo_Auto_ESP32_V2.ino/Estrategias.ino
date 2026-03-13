6a7b4b51289a187bb00fb825871b2efdcccb955e
Isaac Andrade <158227421+alguemaiYT@users.noreply.github.com>
2026-03-13 16:39:22 -0300

    Mudanças minimas de funções

--- Prog AUTO/Sumo_Auto_ESP32_V2.ino/Estrategias.ino
+++ Prog AUTO/Sumo_Auto_ESP32_V2.ino/Estrategias.ino
@@ -21,23 +21,20 @@ void radar(){
   direita(180);
 }}
 }
-void cacador(){
- Le_Sensores();
-  if (sensorLD<=200||sensorLE<=200){
-  re(255);
-  delay(230);}
-  else{
-  if (sen_esq==HIGH) {
-  esquerda(210);
-  }
-  else if (sen_dir==HIGH){
-  direita(210);
-  }
-  else if (sen_centro_esq==HIGH||sen_centro_dir==HIGH)
-  frente(245);
-  else 
-  frente(170);
-  }
+void hunter(){
+  Le_Sensores();
+  if(sensorB==HIGH && sensorC==HIGH)//sensores da frente detectando
+  {frente(veloc*2);}
+  if (sensorB==HIGH && sensorC==LOW)// sensores da frente direita somente detectando
+  {  esquerda(veloc*0.5);}
+  if (sensorB==LOW && sensorC==HIGH)// sensores da dir e esq detectando
+  {  direita(veloc*0.5);}
+  if (sensorA==HIGH)// sensores da frente e esquerda detectando
+  {esquerda(veloc);}
+  if (sensorD==HIGH)// sensores da direita e da frente detectando
+  {direita(veloc);}
+  if (sensorA==LOW && SensorB==LOW && SensorC==LOW && SensorD==LOW) // nenhum sensor detectando
+  frente(veloc/3);
 }
 void firula(){
   digitalWrite(12,HIGH);
